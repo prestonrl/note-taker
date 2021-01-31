5a15022ef0f8d1ada2db7a0b03f5296aea209810
tubasthebest <tubasthebest@gmail.com>
2021-01-31 14:47:44 -0700

    server set up

---
+++ server.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
